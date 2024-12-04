713c1dd50984ac3c8a285e437481facaf05aa4ab
Seth Mwebi <sethmwebi27@gmail.com>
2024-12-04 17:26:58 +0300

    hoisting

---
+++ js/hoisting.js
@@ -0,0 +1,25 @@
+// console.log(number);
+// var number = 5;
+
+// console.log(sayHello());
+//
+// function sayHello() {
+//   return "Hello";
+// }
+
+// function hey() {
+//   console.log(num);
+//   var num = 23;
+// }
+//
+// hey();
+
+// console.log(hey());
+// function hey() {
+//   return "hey";
+// }
+// console.log(num);
+// var num = 5;
+//var name = "Donald"
+// var number = 5;
+// console.log(typeof number);
