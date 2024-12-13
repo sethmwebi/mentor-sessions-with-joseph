ecbb39ec7545aae0f369deb8f4d9ed559cd44c40
Seth Mwebi <sethmwebi27@gmail.com>
2024-12-13 18:05:47 +0300

    arrays

---
+++ js/arrays.js
@@ -0,0 +1,39 @@
+// let students = ["kevin", "mark", "john", 23, true, { name: "mike" }];
+// // console.log(students[5].name);
+// console.log(typeof students);
+let fruits1 = ["strawberry", "apple", "banana", "orange", "passion"];
+let fruits2 = ["guavas", "loquats"];
+// fruits.push("passion");
+// console.log(fruits);
+// fruits.unshift("strawberry");
+// console.log(fruits);
+// fruits[1] = "grapes";
+// console.log(fruits);
+// fruits.splice(3, 1);
+// fruits.splice(1, 2);
+// console.log(fruits);
+// let allFruits = fruits1.concat(fruits2);
+// console.log(allFruits);
+// console.log(fruits1.toString());
+// console.log(fruits1.indexOf("banana"));
+// console.log(
+//   fruits1.find((element) => {
+//     return element.endsWith("n");
+//   }),
+// );
+//
+// console.log(
+//   fruits1.findIndex((element) => {
+//     return element.endsWith("n");
+//   }),
+// );
+// fruits1.forEach((element) => console.log(element));
+// console.log(fruits1.includes("apple"));
+// const sortedFruits = fruits1.sort(function (a, b) {
+//   return a - b;
+// });
+//
+// console.log(sortedFruits);
+// console.log(fruits1.sort().reverse());
+const favoriteFruits = fruits1.splice(1, 1, "guava");
+console.log(fruits1);
