8280121d26f598569653a3dccbcde30e9484e20e
Seth Mwebi <sethmwebi27@gmail.com>
2025-01-13 17:55:12 +0300

    for in and strings

---
+++ js/for-in.js
@@ -0,0 +1,18 @@
+// let president = {
+//   name: "Zelensky",
+//   country: "Ukraine",
+//   kids: 2,
+// };
+//
+// for (let i in president) {
+//   console.log(i);
+// }
+// let president = ["William", 58, "June"];
+// for (let i in president) {
+//   console.log(i);
+// }
+//
+// const numbers = [20, 30, 40];
+let six = Symbol(6);
+let anotherSix = Symbol(6);
+console.log(six === anotherSix);
---
+++ js/strings.js
@@ -0,0 +1,17 @@
+let name = "Donald";
+let president = "Donald";
+
+function quote() {
+  return "We are going to build a wall";
+}
+
+// console.log(`President ${name} once said ${quote()}`);
+// console.log(name[name.length - 1]);
+// name[0] = "J";
+// console.log(name === president);
+// console.log('Kamala is "president"');
+// let number = "" + 1;
+// console.log(typeof number);
+// let isValid = String(true);
+// console.log(typeof isValid);
+console.log("My name is seth.\nI graduated recently.");
